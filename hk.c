5a558b2a2114eeaff1cc3098f0a7dfe44937b1ad
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-01-11 16:57:36 +0530

    Create hk.c

---
+++ hk.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+int a,b,c;
+printf("enter n.o");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+	if(a>c)
+	{
+		printf("%d a is big",a);
+		
+	}}
+	else if(b>c)
+	{
+		printf("%d b is big",b);
+	}
+	else{
+		printf("%d c is big",c);
+	}
+	
+	
