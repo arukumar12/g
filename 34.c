7707c939b90ddbaa3c04bce886188f11c7bf58d1
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-02-06 16:37:34 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+       int i,a=0;
+       char s[100];
+       gets(s);
+       for(i=0;s[i]!=0;i++)
+       {
+           if(s[i]=='.')
+           a++;
+       }
+       printf("%d",a++);
+	return 0;
+}
