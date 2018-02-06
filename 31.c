35b225c7f4bb14c2d322b95739724be7eeb4a76c
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-02-06 16:00:17 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -3,7 +3,7 @@
 int main(void) {
          int i,c=0;
          char a[50];
-         scanf("%s",&a);
+         gets(a);
          for(i=0;a[i]!=0;i++)
          {
          	if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='z'))
