88184d214845a79d79e0378605aaf83880798181
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-02-22 16:37:01 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+	char a[10];
+	char b[10];
+	gets(a);
+	gets(b);
+	if(a[i]==b[i])
+	{
+	    printf("equal");
+	}
+	else
+	{
+	    printf("not equal");
+	}
+	return 0;
+}
