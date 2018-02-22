04630532ec679cc1d5ffa601d833b96ba8acb7d0
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-02-22 16:52:54 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+	char a[10];
+	char b[10];
+	scanf("%s%s",&a,&b);
+	if(a[i]!=b[i])
+	{
+	    printf("%s",a);
+	    printf("%s",b);
+	}
+	return 0;
+}
