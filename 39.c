ac6d728f673250e1d9bf96710db2a86da4be8496
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-02-22 16:06:44 +0530

    Update 39.c

--- 39.c
+++ 39.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int a[10],i,max,j,n;
-scanf("%d",&n);
+int a[10],i,max,j,v;
+scanf("%d",&v);
 printf("\n Enter array");
-for(i=0;i<n;i++)
+for(i=0;i<v;i++)
 {
 scanf("%d",&a[i]);
 }
 max=a[0];
-for(j=1;j<n;j++)
+for(j=1;j<v;j++)
 {
 if(a[j]>max)
  max=a[j];
