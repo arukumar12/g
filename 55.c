9cf94d9577af55340a18003ae6965a8c1b070357
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-03-02 14:40:33 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int n1,n2,res;
+	scanf("%d%d",&n1,&n2);
+	res=n1*n2;
+	if(res%2==0)
+	{
+	    printf("even");
+	}else
+	{
+	    printf("odd");
+	}
+	
+	return 0;
+}
