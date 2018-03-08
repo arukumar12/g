97e3ccedfa9dd261fed28d759811f0d169c00902
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-03-08 16:15:42 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	char a[100];
+	int i,flag;
+	scanf("%s",&a[i]);
+	for(i=0;i<a[i];i++)
+	{
+	if(a[i]=='0'||a[i]=='1')
+	 {
+	      printf("yes");
+	 }
+	else
+	{
+	    printf("no");
+	}
+	}	
+	
+	return 0;
+}
