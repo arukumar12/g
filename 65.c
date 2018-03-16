3d43a6e3c12e98b10081f678b0db7ae743869f4c
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-03-16 13:54:00 +0530

    Update 65.c

--- 65.c
+++ 65.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include<string.h>
 
 int main() {
-	char a[100];
-    scanf("%[^\n]s",a);
-    if(printf("%s",a))
-    {
-        
-    }
-return 0;
+	if(printf("hello world"))
+	{
+	
+	}
+	return 0;
 }
